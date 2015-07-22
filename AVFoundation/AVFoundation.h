6ecd83639b2ad12e799beff3d4b3789df1840c31
Yonghui Chen <chyhfj@gmail.com>
2015-07-22 15:45:21 +0800

    Initial Commit

---
+++ AVFoundation/AVFoundation.h
@@ -0,0 +1,19 @@
+//
+//  AVFoundation.h
+//  AVFoundation
+//
+//  Created by Chen Yonghui on 7/22/15.
+//  Copyright (c) 2015 Shanghai TinyNetwork Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AVFoundation.
+FOUNDATION_EXPORT double AVFoundationVersionNumber;
+
+//! Project version string for AVFoundation.
+FOUNDATION_EXPORT const unsigned char AVFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AVFoundation/PublicHeader.h>
+
+
